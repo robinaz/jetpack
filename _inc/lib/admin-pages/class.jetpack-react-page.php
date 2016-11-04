835ffdeb5c4c515103336631fdefe4d9fb08f377
Igor Zinovyev <zinigor@gmail.com>
2016-11-04 14:57:12 +0300

    Added new flattened settings to the settings reducer.

--- _inc/lib/admin-pages/class.jetpack-react-page.php
+++ _inc/lib/admin-pages/class.jetpack-react-page.php
@@ -266,6 +266,7 @@ class Jetpack_React_Page extends Jetpack_Admin_Page {
 				),
 				'roles' => $stats_roles,
 			),
+			'settings' => $this->get_flattened_settings( $modules ),
 			'settingNames' => array(
 				'jetpack_holiday_snow_enabled' => function_exists( 'jetpack_holiday_snow_option_name' ) ? jetpack_holiday_snow_option_name() : false,
 			),
@@ -283,6 +284,27 @@ class Jetpack_React_Page extends Jetpack_Admin_Page {
 			'tracksUserData' => Jetpack_Tracks_Client::get_connected_user_tracks_identity(),
 			'currentIp' => function_exists( 'jetpack_protect_get_ip' ) ? jetpack_protect_get_ip() : false
 		) );
+	}
+
+	/**
+	 * Returns an array of modules and settings both as first class members of the object.
+	 *
+	 * @param Array $modules the result of an API request to get all modules.
+	 *
+	 * @return Array flattened settings with modules.
+	 */
+	function get_flattened_settings( $modules ) {
+		$settings = array();
+
+		foreach ( $modules as $slug => $data ) {
+			$settings [ $slug ] = $data['activated'] ? true : false;
+
+			foreach ( $data['options'] as $name => $option ) {
+				$settings[ $name ] = $option['current_value'];
+			}
+		}
+
+		return $settings;
 	}
 }
 
